a5cf02e92584a99527d663dbf632a2183aae7157
unknown <miguel.motta-u@labinfo.is.escuelaing.edu.co>
2025-01-30 09:04:19 -0500

    feat: class work

---
+++ src/main/java/primefinder/PrimeFinderThread.java
@@ -0,0 +1,48 @@
+package primefinder;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Scanner;
+
+public class PrimeFinderThread extends Thread{
+
+	
+	int a,b;
+	
+	private List<Integer> primes;
+
+	public PrimeFinderThread(int a, int b) {
+		super();
+                this.primes = new LinkedList<>();
+		this.a = a;
+		this.b = b;
+	}
+
+        @Override
+	public void run(){
+            for (int i= a;i < b;i++){						
+                if (isPrime(i)){
+                    primes.add(i);
+                    System.out.println(i);
+                }
+            }
+	}
+	
+	boolean isPrime(int n) {
+	    boolean ans;
+            if (n > 2) { 
+                ans = n%2 != 0;
+                for(int i = 3;ans && i*i <= n; i+=2 ) {
+                    ans = n % i != 0;
+                }
+            } else {
+                ans = n == 2;
+            }
+	    return ans;
+	}
+
+	public List<Integer> getPrimes() {
+		return primes;
+	}
+	
+}
