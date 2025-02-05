61f2157c9d29745c65b03fb654842a3646ed09bd
Santiago Avellaneda Rodríguez <santiagoavellar02@gmail.com>
2025-02-05 02:14:26 -0500

    fix: fixing concurrency problem while the gui paint the snake

--- src/main/java/snakepackage/Board.java
+++ src/main/java/snakepackage/Board.java
@@ -180,22 +180,24 @@ public class Board extends JLabel implements Observer {
 	}
 
 	private void drawSnake(Graphics g) {
-		for (int i = 0; i != SnakeApp.MAX_THREADS; i++) {
-            LinkedList<Cell> body = SnakeApp.getApp().snakes[i].getBody();
-			for (Cell p : body) {
-				if (p.equals(body.peekFirst())) {
-					g.setColor(new Color(050+(i*10), 205, 150));
-					g.fillRect(p.getX() * GridSize.WIDTH_BOX, p.getY()
-							* GridSize.HEIGH_BOX, GridSize.WIDTH_BOX,
-							GridSize.HEIGH_BOX);
-				} else {
-					if (SnakeApp.getApp().snakes[i].isSelected()) {
-						g.setColor(new Color(032, 178, 170));
-					} else
-						g.setColor(new Color(034, 139, 034));
-					g.fillRect(p.getX() * GridSize.WIDTH_BOX, p.getY()
-							* GridSize.HEIGH_BOX, GridSize.WIDTH_BOX,
-							GridSize.HEIGH_BOX);
+		synchronized (this) {
+			for (int i = 0; i != SnakeApp.MAX_THREADS; i++) {
+				LinkedList<Cell> body = SnakeApp.getApp().snakes[i].getBody();
+				for (Cell p : body) {
+					if (p.equals(body.peekFirst())) {
+						g.setColor(new Color(050 + (i * 10), 205, 150));
+						g.fillRect(p.getX() * GridSize.WIDTH_BOX, p.getY()
+										* GridSize.HEIGH_BOX, GridSize.WIDTH_BOX,
+								GridSize.HEIGH_BOX);
+					} else {
+						if (SnakeApp.getApp().snakes[i].isSelected()) {
+							g.setColor(new Color(032, 178, 170));
+						} else
+							g.setColor(new Color(034, 139, 034));
+						g.fillRect(p.getX() * GridSize.WIDTH_BOX, p.getY()
+										* GridSize.HEIGH_BOX, GridSize.WIDTH_BOX,
+								GridSize.HEIGH_BOX);
+					}
 				}
 			}
 		}
